e022d0d948e9ed5dd1c290e530e3da7ae8624944
Pavel Titov <pavel_titov@mail.ru>
2018-01-16 08:09:03 +0300

    TextChangedListener

--- app/src/main/java/pvtitov/anothertranslator/AddActivity.java
+++ app/src/main/java/pvtitov/anothertranslator/AddActivity.java
@@ -3,9 +3,17 @@ package pvtitov.anothertranslator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Павел on 15.01.2018.
@@ -21,12 +29,36 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        ListView listView = findViewById(R.id.translations_list);
+        List<String> translations = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, translations);
+        listView.setAdapter(adapter);
+
         EditText inputWord = findViewById(R.id.input_word);
-        inputWord.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        inputWord.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                return false;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            private Timer timer = new Timer();
+            private final long DELAY = 500;
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                timer.cancel();
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        translations.add(inputWord.getText().toString());
+                        for (String item: translations) Log.d("happy", item);
+                    }
+                }, DELAY);
             }
         });
+
+
     }
 }
