959fa4590b47df3e2f35229e342933f8a8cf09e4
Pavel Titov <pavel_titov@mail.ru>
2018-01-17 08:17:48 +0300

    touchscreen ломается. Начал реализацию web-клиента

--- app/src/main/java/pvtitov/anothertranslator/AddActivity.java
+++ app/src/main/java/pvtitov/anothertranslator/AddActivity.java
@@ -1,6 +1,8 @@
 package pvtitov.anothertranslator;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -10,10 +12,20 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import pvtitov.anothertranslator.networking.RetrofitFactory;
+import pvtitov.anothertranslator.networking.TranslationModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Павел on 15.01.2018.
@@ -42,20 +54,34 @@ public class AddActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
-            private Timer timer = new Timer();
-            private final long DELAY = 500;
-
+            private final long DELAY = 1000;
+            Handler handler = new Handler(Looper.getMainLooper());
+            Runnable reloadSuggestions;
             @Override
             public void afterTextChanged(Editable s) {
-                timer.cancel();
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        translations.add(inputWord.getText().toString());
-                        for (String item: translations) Log.d("happy", item);
-                    }
-                }, DELAY);
+                handler.removeCallbacks(reloadSuggestions);
+                reloadSuggestions = () -> {
+                    String word = inputWord.getText().toString();
+                    List<String> suggestions;
+
+                    RetrofitFactory.WebService webService = RetrofitFactory.getInstance(BASE_URL).create(RetrofitFactory.WebService.class);
+                    Call<TranslationModel> call = webService.getTranslation(API_KEY, "en-ru", word);
+                    call.enqueue(new Callback<TranslationModel>() {
+                        @Override
+                        public void onResponse(Call<TranslationModel> call, Response<TranslationModel> response) {
+                            List<String> suggestions = response.body().getText();
+                            //TODO доделать получение списка переводов
+                        }
+
+                        @Override
+                        public void onFailure(Call<TranslationModel> call, Throwable t) {
+
+                        }
+                    });
+
+                    adapter.notifyDataSetChanged();
+                };
+                handler.postDelayed(reloadSuggestions, DELAY);
             }
         });
 
